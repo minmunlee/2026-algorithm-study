53c4552c3fc22ac0da93e2d6d1e59fbc7b80e03c
gitseoyeon <tjdus553926@gmail.com>
2026-02-13 18:57:12 +0900

    solve: 값이 k인 트리 노드 깊이

---
+++ 02월/2주차/[BOJ] 값이 k인 트리 노드 깊이/Min.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Min {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(str.nextToken());
+        int k = Integer.parseInt(str.nextToken());
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < n - 1; i++) {
+            str = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(str.nextToken());
+            int c = Integer.parseInt(str.nextToken());
+
+            list.get(p).add(c);
+            list.get(c).add(p);
+        }
+
+        int[] node = new int[n];
+        str = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            node[i] = Integer.parseInt(str.nextToken());
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+
+        queue.offer(0);
+        visited[0] = true;
+        int depth = 0;
+
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+
+            for(int i = 0; i < len; i++) {
+                int cur = queue.poll();
+                if(node[cur] == k) {
+                    System.out.println(depth);
+                    return;
+                }
+                for(int c : list.get(cur)) {
+                    if(!visited[c]) {
+                        visited[c] = true;
+                        queue.offer(c);
+                    }
+                }
+            }
+            depth += 1;
+        }
+    }
+}
