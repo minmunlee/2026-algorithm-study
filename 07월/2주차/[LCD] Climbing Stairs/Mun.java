003f6843d977ad120533680b6aa8f8a3ddf3847a
시원 <105481797+muncool39@users.noreply.github.com>
2026-08-01 19:02:20 +0900

    Mun: 7월 3주차 문제 풀이 (#61)
    * solve: Binary Tree Inorder Traversal
    
    * solve: Merge Sorted Array
    
    * solve: 부대복귀
    
    * solve: Climbing Stairs
    
    * solve: Remove Duplicates from Sorted List

---
+++ 07월/2주차/[LCD] Climbing Stairs/Mun.java
@@ -0,0 +1,15 @@
+class Mun {
+    public int climbStairs(int n) {
+        if(n <= 2) {
+            return n;
+        }
+        int p1 = 1;
+        int p2 = 2;
+        for(int i=3;i<=n;i++) {
+            int sum = p1 + p2;
+            p1 = p2;
+            p2 = sum;
+        }
+        return p2;
+    }
+}
